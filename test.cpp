9e25cb8b90f090006ec7ad4742d6eb4d2a73830d
EmanOthman21 <eothman21@gmail.com>
2022-05-07 15:05:00 +0200

    test(Phase 1): :test_tube: Add test file

---
+++ test.cpp
@@ -0,0 +1,78 @@
+int i = 5;
+float f = 5.5;
+bool b = true;
+string s = "Hello";
+
+const int ci = 5;
+const float cf = 5.5;
+const bool cb = true;
+const string cs = "Hello";
+
+bool a = true && false || (flag  ^ ture);
+
+y = y + x;
+x = x + 2;
+flag = true;
+
+print x+5;
+
+while(i < 10)
+{
+    i = i + 1;
+    f = f * 2;
+
+    if (b == true) {
+        b = !b;
+    } else {
+        b = !b;
+    }
+}
+
+
+if (b == true) {
+    s = "Hello";
+} else {
+    s = "World";
+}
+
+for (int i = 0; i < 10; i = i + 1) {
+    i = i + 1;
+    cs = "Const hello";
+}
+
+for (; i < 10; i = i + 1) {
+    if (i % 2 == 0) {
+        b = !b;
+        f = f * 2;
+    }
+
+}
+
+{
+    cf = cf * 2;
+    cb = !cb;
+}
+
+for(;;) {
+    bool flag1 = true;
+}
+
+repeat {
+    x = x +2;
+} until (x <100);
+
+switch (x) {
+    case 1: {
+        x = x + 1;
+        break;
+    }
+
+    case 2:
+        break;
+
+    case 3:
+        flag = false;
+
+    default:
+        x = 100;    
+}
